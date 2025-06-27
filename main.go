294f282a7336bf7fd64766100b551fc70952d237
cor0nius <jacekglowacki94@gmail.com>
2025-06-27 20:45:56 +0200

    added hits counter

--- main.go
+++ main.go
@@ -3,20 +3,42 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
 	serveMux := http.NewServeMux()
 	server := http.Server{Handler: serveMux}
 	server.Addr = ":8080"
-	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("app"))))
+	apiCfg := &apiConfig{}
+	apiCfg.fileserverHits.Store(0)
+	serveMux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir("app")))))
 	serveMux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
 	})
+	serveMux.HandleFunc("/metrics", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		fmt.Fprintf(w, "Hits: %v", apiCfg.fileserverHits.Load())
+	})
+	serveMux.HandleFunc("/reset", func(w http.ResponseWriter, req *http.Request) {
+		apiCfg.fileserverHits.Store(0)
+	})
 	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Print(err)
 	}
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
